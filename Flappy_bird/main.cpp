42fb0092cca9172f11e96f6e4a46bcfe92ed1546
Andrew Raiden <sk.quirtel@gmail.com>
2015-05-10 22:16:58 +0600

    Добавлены тайлы

--- Flappy_bird/main.cpp
+++ Flappy_bird/main.cpp
@@ -1,11 +1,34 @@
 #include <SFML\Graphics.hpp>
+#include <string>
 
 using namespace sf;
 
 int ground = 350;
 
+float offsetX=0, offsetY=0;
+
 const int H = 16;
-const int W = 148;
+const int W = 54;
+
+String TileMap[H] = {
+	"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+	"B              B                                     B",
+	"B              B                                     B",
+	"B              B                                     B",
+	"B              B                                     B",
+	"B              B                                     B",
+	"B              B                                     B",
+	"B              B                                     B",
+	"B              B                                     B",
+	"B              B                                     B",
+	"B              B                                     B",
+	"B                                                    B",
+	"B                                                    B",
+	"B                                                    B",
+	"HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
+	"HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
+}; 
+
 
 class player {
 
@@ -19,20 +42,20 @@ public:
 	player(Texture &image){
 		sprite.setTexture(image);
 		sprite.setTextureRect(IntRect(3,5,19,15));
-		rect = FloatRect(0,0,0,0);
+		rect = FloatRect(0,-300,-50,-300);
 		dx=0;
 		dy=0;
 		current_frame = 0;
 	}
 
 	void update(float time){
-		dx = 0.008; // скорость перемещени€
+		dx = 0.08; // скорость перемещени€
 		rect.left += dx*time; // посто€нное перемещение вправо
-		
+		Collision(0);
 		// гравитаци€
-		dy = dy + 0.00005 * time;
+		dy = dy + 0.0005 * time;
 		rect.top += dy*time;
-
+		Collision(1);
 		// не даЄт выйти за платформу
 		if(rect.top > ground) {
 			rect.top = ground;
@@ -40,34 +63,41 @@ public:
 			on_ground = true;
 		}
 
+
 		// вывод
-		sprite.setPosition(rect.left, rect.top);
+		sprite.setPosition(rect.left-offsetX+50, rect.top - offsetY+80);
 	}
+
+	void Collision(int dir)
+   {
+     for (int i = rect.top/32 ; i<(rect.top+rect.height)/32; i++)
+	  for (int j = rect.left/32; j<(rect.left+rect.width)/32; j++)
+		{ 
+	  	 if (TileMap[i][j]=='B') 
+		   { 
+	        if ((dx>0) && (dir==0)) rect.left =  j*32 -  rect.width; 
+		    if ((dx<0) && (dir==0)) rect.left =  j*32 + 32;
+			if ((dy>0) && (dir==1))  { rect.top =   i*32 -  rect.height/2;}
+			if ((dy<0) && (dir==1))  { rect.top = i*32 + 60; }
+		   }
+		 
+		 if (TileMap[i][j]=='H') 
+		                   { 
+			                 TileMap[i][j]=' ';
+		                   }
+	         	
+    	}
+   
+   }
+
+
 };
 
-String TileMap[H] = {
-	"00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-	"0                                                                                                                                                   0",
-	"0             0                                                                                                                                     0",
-	"0                                                                                                                                                   0",
-	"0                                                                                                                                                   0",
-	"0                                                                                                                                                   0",
-	"0                                  P                                                                                                                0",
-	"0                                                                                                                                                   0",
-	"0                                                                                                                                                   0",
-	"0                                                                                                                                                   0",
-	"0                                                                                                                                                   0",
-	"0                                                                                                                                                   0",
-	"0                                                                                                                                                   0",
-	"0                                                                                                                                                   0",
-	"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
-	"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
-}; 
 
 
 void main () {
 
-	RenderWindow window (VideoMode(600,400), "Flappy Bird");  
+	RenderWindow window (VideoMode(1000,500), "Flappy Bird");  
 
 	Texture t;
 	t.loadFromFile("FLAPPYRED.png");
@@ -76,7 +106,9 @@ void main () {
 
 	Clock clock;
 
-	RectangleShape rectangle;
+	RectangleShape rectangle(Vector2f(32,32));
+
+
 
 	while (window.isOpen()){
 
@@ -94,29 +126,38 @@ void main () {
 
 
 		if (Keyboard::isKeyPressed(Keyboard::Up)){
-			bird.dy = -0.01;
+			bird.dy = -0.25;
 		}
 
 		bird.update(time);
-		window.clear(Color::White);
 
+		offsetX = bird.rect.left;
+
+
+
+		window.clear(Color::White);
 
 		for (int i=0; i<H;i++){
 			for (int j=0; j<W;j++) {
 
-				if (TileMap[i][j] == '0'){ 
-					rectangle.setFillColor(sf::Color::Black);}
-				if (TileMap[i][j] == '–') {
-					rectangle.setFillColor(sf::Color::Red);}
+				if (TileMap[i][j] == 'B'){ 
+					rectangle.setFillColor(sf::Color::Green);
+				}
+				if (TileMap[i][j] == 'H') {
+					rectangle.setFillColor(sf::Color::Red);
+				}
 				if (TileMap[i][j] == ' ') {
-					continue;}
+					rectangle.setFillColor(sf::Color::White);
+				}
 
-				rectangle.setPosition(j*10,i*10);
+				rectangle.setPosition(j*32-offsetX,i*32-offsetY);
 				window.draw(rectangle);
 			}
 
 		}
-		
+
+
+
 		window.draw(bird.sprite);
 		window.display();
 	}

3f24bdc9400c3a62f301c6c9c149e9af16985bf5
Daniil159x <daniil.ru-m@mail.ru>
2015-05-11 18:08:24 +0600

    Сделал класс карт, добавил отображение текстур

--- Flappy_bird/main.cpp
+++ Flappy_bird/main.cpp
@@ -1,33 +1,18 @@
 #include <SFML\Graphics.hpp>
 #include <string>
+#include "maps.h"
+
 
 using namespace sf;
 
-int ground = 350;
+int ground = 231;
 
 float offsetX=0, offsetY=0;
 
-const int H = 16;
-const int W = 54;
+const int bit = 12;
 
-String TileMap[H] = {
-	"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
-	"B              B                                     B",
-	"B              B                                     B",
-	"B              B                                     B",
-	"B              B                                     B",
-	"B              B                                     B",
-	"B              B                                     B",
-	"B              B                                     B",
-	"B              B                                     B",
-	"B              B                                     B",
-	"B              B                                     B",
-	"B                                                    B",
-	"B                                                    B",
-	"B                                                    B",
-	"HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
-	"HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH",
-}; 
+// создание окна
+RenderWindow window (VideoMode(1000,500), "Flappy Bird");
 
 
 class player {
@@ -42,7 +27,7 @@ public:
 	player(Texture &image){
 		sprite.setTexture(image);
 		sprite.setTextureRect(IntRect(3,5,19,15));
-		rect = FloatRect(0,-300,-50,-300);
+		rect = FloatRect(0,0,0,0);
 		dx=0;
 		dy=0;
 		current_frame = 0;
@@ -50,12 +35,27 @@ public:
 
 	void update(float time){
 		dx = 0.08; // скорость перемещени€
-		rect.left += dx*time; // посто€нное перемещение вправо
-		Collision(0);
+		rect.left += dx*time; // посто€нное перемещение вправо 
+		
+		
+		
+		// анимаци€
+		current_frame += 0.015;
+		if (current_frame > 3) {current_frame = 0;}
+
+		if (dy < 0){
+			switch(int(current_frame)){
+				case 1: {sprite.setTextureRect(IntRect(21,5,19,15));break;}
+				case 2: {sprite.setTextureRect(IntRect(39,5,19,15));break;}
+				case 0: {sprite.setTextureRect(IntRect(3,5,19,15));break;}
+			}
+		}
+		
 		// гравитаци€
 		dy = dy + 0.0005 * time;
 		rect.top += dy*time;
-		Collision(1);
+		
+
 		// не даЄт выйти за платформу
 		if(rect.top > ground) {
 			rect.top = ground;
@@ -68,7 +68,7 @@ public:
 		sprite.setPosition(rect.left-offsetX+50, rect.top - offsetY+80);
 	}
 
-	void Collision(int dir)
+	/*void Collision(int dir)
    {
      for (int i = rect.top/32 ; i<(rect.top+rect.height)/32; i++)
 	  for (int j = rect.left/32; j<(rect.left+rect.width)/32; j++)
@@ -88,7 +88,7 @@ public:
 	         	
     	}
    
-   }
+   }*/
 
 
 };
@@ -97,8 +97,6 @@ public:
 
 void main () {
 
-	RenderWindow window (VideoMode(1000,500), "Flappy Bird");  
-
 	Texture t;
 	t.loadFromFile("FLAPPYRED.png");
 
@@ -106,21 +104,30 @@ void main () {
 
 	Clock clock;
 
-	RectangleShape rectangle(Vector2f(32,32));
+	Sprite tile(t);
+	Sprite Fon(t);
 
-
-
+	int PosI = 4, PosJ = 50;
+	
 	while (window.isOpen()){
-
+		
+		
 		float time = clock.getElapsedTime().asMicroseconds();
 		clock.restart();
 
 		time = time/900;
+		
+		/*
+		map level[1];
+		for (int i = 0; i < H; i++){
+			level[0].Map[i] = TileMap[i];
+		}*/
 
+		
 		Event event;
 		while (window.pollEvent(event)){
 
-			if (event.type == Event::Closed){ 
+		if (event.type == Event::Closed){ 
 				window.close(); }
 		}
 
@@ -128,37 +135,47 @@ void main () {
 		if (Keyboard::isKeyPressed(Keyboard::Up)){
 			bird.dy = -0.25;
 		}
-
+		
 		bird.update(time);
 
 		offsetX = bird.rect.left;
 
-
-
 		window.clear(Color::White);
-
+		
+		
 		for (int i=0; i<H;i++){
-			for (int j=0; j<W;j++) {
+			for (int j = 0, J = 0; j < W; j++, J++) {
 
-				if (TileMap[i][j] == 'B'){ 
-					rectangle.setFillColor(sf::Color::Green);
+				/* ѕотом доделаю
+				// загрузка 1 сло€ - фона
+				if((4+J*bit) < 136){
+					Fon.setTextureRect(IntRect(4+J*bit,50+i*bit,12,12));}
+				else {J = 0; }
+				
+				Fon.setPosition(j*bit,i*bit);
+				window.draw(Fon);
+				*/
+				
+				// загрузка 2 сло€ - объектов(плиток)
+				switch (level[0].Tile[i][j]){
+					case 'Q': { tile.setTextureRect(IntRect(150,106,12,12)); break;}
+					case 'W': { tile.setTextureRect(IntRect(150,121,12,12)); break;}
+					case 'L': { tile.setTextureRect(IntRect(149,65,12,12)); break;}
+					case 'R': { tile.setTextureRect(IntRect(163,65,12,12)); break;}
+					case 'l': { tile.setTextureRect(IntRect(149,51,12,12)); break;}
+					case 'r': { tile.setTextureRect(IntRect(163,51,12,12)); break;}
+					case 'Z': { tile.setTextureRect(IntRect(149,77,12,-12)); break;}
+					case 'X': { tile.setTextureRect(IntRect(163,77,12,-12)); break;}
+					default: { tile.setTextureRect(IntRect(0,0,0,0));}
 				}
-				if (TileMap[i][j] == 'H') {
-					rectangle.setFillColor(sf::Color::Red);
-				}
-				if (TileMap[i][j] == ' ') {
-					rectangle.setFillColor(sf::Color::White);
-				}
-
-				rectangle.setPosition(j*32-offsetX,i*32-offsetY);
-				window.draw(rectangle);
+				
+				tile.setPosition(j*bit-offsetX,i*bit-offsetY);
+				window.draw(tile);
 			}
-
 		}
-
-
-
+		
 		window.draw(bird.sprite);
-		window.display();
+		window.display(); 
 	}
+	
 }
---
+++ Flappy_bird/maps.h
@@ -0,0 +1,63 @@
+#include <SFML\Graphics.hpp>
+
+
+
+using namespace sf;
+
+const int H = 23; // высота
+const int W = 54; // длина
+
+const int num_maps = 1;
+
+/*
+		Q = 149,105,12,12 - граница платформы
+		W = 149,120,12,12 - продолжение платформы
+		l = 149,51,12,12 - левая сторона трубы
+		r = 163,51,12,12 - правая сторона трубы
+		L = 149,65,12,12 - левая верхняя сторона трубы
+		R = 163,65,12,12 - правая верхняя сторона трубы
+		Z = 149,77,12,-12 - левая нижняя сторона трубы
+		X = 163,77,12,-12 - правая нижняя сторона трубы
+
+*/
+
+String TileMap[H] = {
+		"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+		"B             lr          lr                                   B",
+		"B             lr          lr                                   B",	
+		"B             lr          lr                                   B",
+		"B             LR          lr                                   B",
+		"B                         lr                                   B",
+		"B                         lr                                   B",
+		"B                         lr                                   B",
+		"B                         LR                                   B",
+		"B                                                              B",
+		"B                                                              B",
+		"B                                                              B",
+		"B                                                              B",
+		"B             ZX                                               B",
+		"B             lr                                               B",
+		"B             lr                                               B",
+		"B             lr                                               B",
+		"B             lr          ZX                                   B",
+		"B             lr          lr                                   B",
+		"B             lr          lr                                   B",
+		"QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ",
+		"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+		"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+	};
+
+
+
+class map {
+public:
+	String *Tile;
+
+	map(String *m){
+		Tile = m;
+	}
+
+} 
+// в фигурных скобках перечень карт, их количесво должно соответсвовать с num_maps
+// num_maps можно увеличивать
+level[num_maps] = {TileMap};

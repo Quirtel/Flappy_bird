86fc006d3e1aec54ac1d1f7a3e3b9cd376a59e3f
Daniil159x <daniil.ru-m@mail.ru>
2015-05-13 20:51:51 +0600

    Сделан обработчик столкновения и появления GAME OVER

--- Flappy_bird/main.cpp
+++ Flappy_bird/main.cpp
@@ -1,18 +1,23 @@
 #include <SFML\Graphics.hpp>
 #include <string>
 #include "maps.h"
+#include <iostream>
 
 
 using namespace sf;
 
-int ground = 231;
+int ground = 227;
 
-float offsetX=0, offsetY=0;
+float offsetX = 0;
+int start_p_x = 50;
+int start_p_y = 114;
+bool key_pressed = false;
+
 
 const int bit = 12;
 
 // создание окна
-RenderWindow window (VideoMode(1000,500), "Flappy Bird");
+RenderWindow window (VideoMode(700,500), "Flappy Bird");
 
 
 class player {
@@ -20,25 +25,36 @@ class player {
 public:
 	float dx, dy;
 	FloatRect rect;
-	bool on_ground; //где игрок: земля - воздух
+	bool death_p; // смерть: True - живой; False - мервыё
 	Sprite sprite; //текстурка
 	float current_frame; //текущий фрейм
 
+
+//-------------------------
 	player(Texture &image){
 		sprite.setTexture(image);
 		sprite.setTextureRect(IntRect(3,5,19,15));
-		rect = FloatRect(0,0,0,0);
+		rect = FloatRect(0,0,19,15);
 		dx=0;
 		dy=0;
 		current_frame = 0;
+		death_p = true;
 	}
-
+//**************************
 	void update(float time){
+		if (!(rect.top == 0 && rect.left == 0)){
+			start_p_y = 0;
+		}
+
+		if (death_p == true){
 		dx = 0.08; // скорость перемещения
 		rect.left += dx*time; // постоянное перемещение вправо 
+		}
+		else { dx = 0; }
 		
+		// обработка столкновения по Х
 		
-		
+				
 		// анимация
 		current_frame += 0.015;
 		if (current_frame > 3) {current_frame = 0;}
@@ -51,55 +67,90 @@ public:
 			}
 		}
 		
+
+		// не даёт выйти за верхнию грницу
+		if(rect.top < bit){
+			rect.top = bit;
+			dy = 0;
+		}
+
+		// для позиции смерти
+		float rect_top_last = rect.top;
+
 		// гравитация
 		dy = dy + 0.0005 * time;
 		rect.top += dy*time;
+		rect.top += start_p_y;
 		
+		// обработка стлкновения по Y
+		if (death_p == true) {
+			CollisionY(rect_top_last);
+		}
 
-		// не даёт выйти за платформу
+		// не даёт выйти за нижнюю границу
 		if(rect.top > ground) {
 			rect.top = ground;
 			dy = 0;
-			on_ground = true;
+			death_p = false;
 		}
 
-
 		// вывод
-		sprite.setPosition(rect.left-offsetX+50, rect.top - offsetY+80);
+		sprite.setPosition(rect.left-offsetX+start_p_x, rect.top);
+
 	}
 
-	/*void Collision(int dir)
-   {
-     for (int i = rect.top/32 ; i<(rect.top+rect.height)/32; i++)
-	  for (int j = rect.left/32; j<(rect.left+rect.width)/32; j++)
-		{ 
-	  	 if (TileMap[i][j]=='B') 
-		   { 
-	        if ((dx>0) && (dir==0)) rect.left =  j*32 -  rect.width; 
-		    if ((dx<0) && (dir==0)) rect.left =  j*32 + 32;
-			if ((dy>0) && (dir==1))  { rect.top =   i*32 -  rect.height/2;}
-			if ((dy<0) && (dir==1))  { rect.top = i*32 + 60; }
-		   }
-		 
-		 if (TileMap[i][j]=='H') 
-		                   { 
-			                 TileMap[i][j]=' ';
-		                   }
-	         	
-    	}
-   
-   }*/
+
+//*********************
+	void CollisionY(float p) {
+
+		int X1 = (rect.left+start_p_x) / bit;
+		int Y1 = rect.top / bit;
+		int X2 = (rect.left  + start_p_x + bit) / bit;
+		int Y2 = (rect.top + bit) / bit;
+		int X3 = (rect.left  + start_p_x + bit) / bit;
+		int Y3 = rect.top / bit;
+		int X4 = (rect.left+start_p_x) / bit;
+		int Y4 = (rect.top + bit) / bit;
+
+		if (level[0].Tile[Y1][X1] == 'B') {
+			std::cout << "CollisionY with 'B' " << std::endl;
+		}
+		if (level[0].Tile[Y1][X1] == 'l' || level[0].Tile[Y1][X1] == 'r' || level[0].Tile[Y1][X1] == 'L' || 
+			level[0].Tile[Y1][X1] == 'R' || level[0].Tile[Y1][X1] == 'Z' || level[0].Tile[Y1][X1] == 'X' ||
+			level[0].Tile[Y1][X1] == 'Q'){
+				std::cout << "Collision point (X1; Y1) " << std::endl;
+				rect.top = p;
+				death_p = false;
+		}
+		if  (level[0].Tile[Y2][X2] == 'l' || level[0].Tile[Y2][X2] == 'r' || level[0].Tile[Y2][X2] =='L' ||
+			level[0].Tile[Y2][X2] =='R' ||level[0].Tile[Y2][X2] == 'Z' || level[0].Tile[Y2][X2] == 'X' || 
+			level[0].Tile[Y2][X2] =='Q') {
+				std::cout << "Collision point (X2; Y2) " << std::endl;
+				rect.top = p;
+				death_p = false;
+		}
+		if  (level[0].Tile[Y3][X3] == 'l' || level[0].Tile[Y3][X3] == 'r' || level[0].Tile[Y3][X3] =='L' ||
+			level[0].Tile[Y3][X3] =='R' ||level[0].Tile[Y3][X3] == 'Z' || level[0].Tile[Y3][X3] == 'X' || 
+			level[0].Tile[Y3][X3] =='Q') {
+				std::cout << "Collision point (X3; Y3) " << std::endl;
+				rect.top = p;
+				death_p = false;
+		}
+		if  (level[0].Tile[Y4][X4] == 'l' || level[0].Tile[Y4][X4] == 'r' || level[0].Tile[Y4][X4] =='L' ||
+			level[0].Tile[Y4][X4] =='R' ||level[0].Tile[Y4][X4] == 'Z' || level[0].Tile[Y4][X4] == 'X' || 
+			level[0].Tile[Y4][X4] =='Q') {
+				std::cout << "Collision point (X4; Y4) " << std::endl;
+				rect.top = p;
+				death_p = false;
+		}
+
+	};
 
 
+//****
 };
 
-
-
-void main () {
-
-	Texture t;
-	t.loadFromFile("FLAPPYRED.png");
-
+void algorithm_gemas (Texture &t){
 	player bird(t);
 
 	Clock clock;
@@ -115,30 +166,33 @@ void main () {
 		float time = clock.getElapsedTime().asMicroseconds();
 		clock.restart();
 
-		time = time/900;
-		
-		/*
-		map level[1];
-		for (int i = 0; i < H; i++){
-			level[0].Map[i] = TileMap[i];
-		}*/
-
+		time = time/1500;
 		
 		Event event;
 		while (window.pollEvent(event)){
 
-		if (event.type == Event::Closed){ 
+			if (event.type == Event::Closed){ 
 				window.close(); }
 		}
 
+		if (bird.death_p == true) {
+			if (Keyboard::isKeyPressed(Keyboard::Up) == true && key_pressed == false ){ 
+				bird.dy = -0.25;
+				key_pressed = true;
+			}
 
-		if (Keyboard::isKeyPressed(Keyboard::Up)){
-			bird.dy = -0.25;
+			if (Keyboard::isKeyPressed(Keyboard::Up) == false){ 
+				key_pressed = false;
+			}
+		}
+		else {
+			bird.dy = 0.25;
 		}
 		
-		bird.update(time);
-
 		offsetX = bird.rect.left;
+
+		bird.update(time); 
+		
 
 		window.clear(Color::White);
 		
@@ -146,36 +200,54 @@ void main () {
 		for (int i=0; i<H;i++){
 			for (int j = 0, J = 0; j < W; j++, J++) {
 
-				/* Потом доделаю
+				
 				// загрузка 1 слоя - фона
 				if((4+J*bit) < 136){
 					Fon.setTextureRect(IntRect(4+J*bit,50+i*bit,12,12));}
 				else {J = 0; }
 				
-				Fon.setPosition(j*bit,i*bit);
+				Fon.setPosition(j*bit-offsetX,i*bit);
 				window.draw(Fon);
-				*/
+				
 				
 				// загрузка 2 слоя - объектов(плиток)
 				switch (level[0].Tile[i][j]){
-					case 'Q': { tile.setTextureRect(IntRect(150,106,12,12)); break;}
-					case 'W': { tile.setTextureRect(IntRect(150,121,12,12)); break;}
-					case 'L': { tile.setTextureRect(IntRect(149,65,12,12)); break;}
-					case 'R': { tile.setTextureRect(IntRect(163,65,12,12)); break;}
-					case 'l': { tile.setTextureRect(IntRect(149,51,12,12)); break;}
-					case 'r': { tile.setTextureRect(IntRect(163,51,12,12)); break;}
-					case 'Z': { tile.setTextureRect(IntRect(149,77,12,-12)); break;}
-					case 'X': { tile.setTextureRect(IntRect(163,77,12,-12)); break;}
+					case 'Q': { tile.setTextureRect(IntRect(150,106,bit,bit)); break;}
+					case 'W': { tile.setTextureRect(IntRect(150,121,bit,bit)); break;}
+					case 'L': { tile.setTextureRect(IntRect(149,65,bit,bit)); break;}
+					case 'R': { tile.setTextureRect(IntRect(163,65,bit,bit)); break;}
+					case 'l': { tile.setTextureRect(IntRect(149,51,bit,bit)); break;}
+					case 'r': { tile.setTextureRect(IntRect(163,51,bit,bit)); break;}
+					case 'Z': { tile.setTextureRect(IntRect(149,77,bit,-bit)); break;}
+					case 'X': { tile.setTextureRect(IntRect(163,77,bit,-bit)); break;}
+					case 'B': { tile.setTextureRect(IntRect(150,117,bit,-bit)); break;}
 					default: { tile.setTextureRect(IntRect(0,0,0,0));}
 				}
 				
-				tile.setPosition(j*bit-offsetX,i*bit-offsetY);
+				tile.setPosition(j*bit-offsetX,i*bit);
 				window.draw(tile);
 			}
 		}
+
+		// отображение GAME OVER
+		if (bird.death_p == false){
+			tile.setTextureRect(IntRect(59,0,93,18));
+			tile.setPosition(start_p_x + 50,110);
+			window.draw(tile);
+		}
 		
 		window.draw(bird.sprite);
-		window.display(); 
+		window.display();
+
 	}
+}
+
+
+void main () {
+
+	Texture t;
+	t.loadFromFile("FLAPPYRED.png");
+
+	algorithm_gemas(t);
 	
 }
